544988d721f8829571ed7744ce0e31553a4584c5
Emily Wang <lunareclipsite@gmail.com>
2016-12-28 22:14:56 -0500

    Fixed initialization, added Attack

--- NormalRPG.java
+++ NormalRPG.java
@@ -1,41 +1,75 @@
 public class NormalRPG{
-    private String[] Demo;
-    private String[] Slime;
+    private int[] Demo;
+    private int[] Slime;
  
     public NormalRPG(){
-      Demo[10];
-      Slime[10];
+      Demo = new int[10];
+      Slime = new int[10];
     }
     
-    public int getlvl(String[] a) {
+    public int getlvl(int[] a) {
       return a[0];
     }
-    public int gethp(String[] a){
+    public int gethp(int[] a){
       return a[1];
     }
-    public int getatttack(String[] a){
+    public int getattack(int[] a){
       return a[2];
     }
-    public int getdef(String[] a){
+    public int getdef(int[] a){
       return a[3];
     }
-    public int getspd(String[] a){
+    public int getspd(int[] a){
       return a[4];
     }
     
-    public int setlvl(String[] a, int b){
+    public void setlvl(int[] a, int b){
       a[0] = b;
     }
-    public int sethp(String[] a, int b){
+    public void sethp(int[] a, int b){
       a[1] = b;
     }
-    public int setattack(String[] a, int b){
+    public void setattack(int[] a, int b){
       a[2] = b;
     }
-    public int setdef(String[] a, int b){
+    public void setdef(int[] a, int b){
       a[3] = b;
     }
-    public int setspd(String[] a, int b){
+    public void setspd(int[] a, int b){
       a[4] = b;
     }
-}
+    
+    public String Attack(int[] attacker, int[] target){
+      int c = (int)(Math.random() * 20);
+      if (c == 0){
+        return "MISS";
+      }
+      if (c == 19){
+        int z = (int)(getattack(attacker) * 1.5) - (getdef(target));
+        if (z < 0) {
+          z = 0;
+        }
+        sethp(target, gethp(target) - z);
+        if (gethp(target) < 0){
+          sethp(target, 0);
+        }
+        return "CRITICAL HIT:" + z + " damage dealt";
+      }
+      else {
+        int z = (int)(getattack(attacker) - (getdef(target)));
+        if (z < 0) {
+          z = 0;
+        }
+        sethp(target, gethp(target) - z);
+        if (gethp(target) < 0){
+          sethp(target, 0);
+        }
+        return "" + z + " damage dealt";
+      }
+    }
+          
+                                           
+    }
+    
+    
+
